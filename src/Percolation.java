f3b61ecfab96105cdb01ab757162a5cf5c4bf5b2
Anton Astashkin (Nikolaev) <a.astashkin@mobidev.biz>
2017-09-05 18:11:33 +0300

    percolation problem template

---
+++ src/Percolation.java
@@ -0,0 +1,18 @@
+public class Percolation {
+
+    public Percolation(int N) {
+
+    }
+
+    public boolean connected(int p, int q) {
+        return true;
+    }
+
+    public void union(int p, int q) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
